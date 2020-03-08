352598a4e4ad92e1da8b003d6e250b24261b70c1
Andrey <a.so777@live.ca>
2020-03-07 21:25:27 -0800

    Update Invest.js

--- src/features/investCalc/Invest.js
+++ src/features/investCalc/Invest.js
@@ -1,26 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { Card, Form, Button, Radio, Input, Label } from "semantic-ui-react";
+import { Card, Form, Button, Radio, Input, Label, Message, Icon } from "semantic-ui-react";
 
 const Invest = () => {
   const [startAmount, setStartAmount] = useState(0);
   const [startAmountError, setStartAmountError] = useState(false);
 
   const [years, setYears] = useState(0);
-  const [yearsError, setYearsError] = useState(0);
+  const [yearsError, setYearsError] = useState(false);
 
   const [returnRate, setReturnRate] = useState(0);
-  const [returnRateError, setReturnRateError] = useState(0);
+  const [returnRateError, setReturnRateError] = useState(false);
 
   const [contribution, setContribution] = useState(0);
-  const [contributionError, setContributionError] = useState(0);
+  const [contributionError, setContributionError] = useState(false);
 
   const [contributionFrq, setContributionFrq] = useState("week");
 
-  const calculateInvestment = () => {};
+  const [investAmount, setInvestAmount] = useState(0);
+
+  const calculateInvestment = () => {
+    let amount = 0;
+    amount = startAmount*(1+(returnRate/100*years));
+    if (!!isNaN(amount) || amount >= 0) {
+      setInvestAmount(amount);
+    }
+  };
 
   useEffect(() => {
-    console.log(startAmount);
-  }, [startAmount]);
+    calculateInvestment();
+  }, [startAmount, years, returnRate, contribution, contributionFrq]);
 
   return (
     <div>
@@ -29,7 +37,11 @@ const Invest = () => {
           <Card.Header>Investment Calculator</Card.Header>
         </Card.Content>
         <Card.Content extra>
-          <Form>
+        <Message attached="bottom" success>
+            <Icon name="dollar" />
+            You will have ${investAmount} within {years} years.
+          </Message>
+          <Form className="attached">
             <Form.Field>
               <label>Starting amount</label>
               <Input
@@ -39,7 +51,7 @@ const Invest = () => {
                 value={startAmount}
                 error={startAmountError}
                 onChange={(e, { name, value }) => {
-                  if (!isNaN(value) || value > 0) {
+                  if (!isNaN(value) || value >= 0) {
                     setStartAmount(value);
                     setStartAmountError(false);
                   } else {
@@ -54,11 +66,40 @@ const Invest = () => {
             </Form.Field>
             <Form.Field>
               <label>After (in years)</label>
-              <input />
+              <Input
+                type="text"
+                placeholder="Years"
+                value={years}
+                error={yearsError}
+                onChange={(e, { name, value }) => {
+                  if (!isNaN(value) || value > 0) {
+                    setYears(value);
+                    setYearsError(false);
+                  } else {
+                    setYearsError(true);
+                  }
+                }}
+              >
+                <input />
+              </Input>
             </Form.Field>
             <Form.Field>
               <label>Return rate</label>
-              <Input labelPosition="right" type="text" placeholder="Percent">
+              <Input
+                labelPosition="right"
+                type="text"
+                placeholder="Percent"
+                value={returnRate}
+                error={returnRateError}
+                onChange={(e, { name, value }) => {
+                  if (!isNaN(value) || value > 0) {
+                    setReturnRate(value);
+                    setReturnRateError(false);
+                  } else {
+                    setReturnRateError(true);
+                  }
+                }}
+              >
                 <Label basic>%</Label>
                 <input />
                 <Label>.00</Label>
@@ -66,7 +107,21 @@ const Invest = () => {
             </Form.Field>
             <Form.Field>
               <label>Additional contribution</label>
-              <Input labelPosition="right" type="text" placeholder="Amount">
+              <Input
+                labelPosition="right"
+                type="text"
+                placeholder="Amount"
+                value={contribution}
+                error={contributionError}
+                onChange={(e, { name, value }) => {
+                  if (!isNaN(value) || value > 0) {
+                    setContribution(value);
+                    setContributionError(false);
+                  } else {
+                    setContributionError(true);
+                  }
+                }}
+              >
                 <Label basic>$</Label>
                 <input />
                 <Label>.00</Label>
@@ -97,10 +152,10 @@ const Invest = () => {
                 }}
               />
             </Form.Field>
-            <Button type="submit">Calculate</Button>
           </Form>
         </Card.Content>
       </Card>
+
     </div>
   );
 };
